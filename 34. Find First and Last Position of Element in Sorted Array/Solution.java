783f8946863c8700a80c882844954b2a42c6f51c
Codey <forthecodeman@gmail.com>
2023-10-08 22:54:48 -0400

    Solved 34
    Also added new file format

---
+++ 34. Find First and Last Position of Element in Sorted Array/Solution.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] toReturn = {-1, -1}; //initialize to {-1, -1}
+        //1st pass, search below
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int mid = ((r + l) / 2);
+            if (nums[mid] > target) r = mid - 1;
+            if (nums[mid] < target) l = mid + 1;
+            if (nums[mid] == target) {
+                if (mid == 0 || nums[mid - 1] != target) {
+                    toReturn[0] = mid;
+                    break;
+                } else {
+                    r = mid;
+                } //if         
+            } ///if
+        } //while
+
+        //2nd pass, but search above
+        l = 0;
+        r = nums.length - 1;
+        while (l <= r) {
+            int mid = ((r + l) / 2);
+            //System.out.println("l:" + l + " r:" + r + " mid:" + mid);
+            if (nums[mid] > target) r = mid - 1;
+            if (nums[mid] < target) l = mid + 1;
+            if (nums[mid] == target) {
+                if (mid == nums.length - 1 || nums[mid + 1] != target) {
+                    toReturn[1] = mid;
+                    break;
+                } else {
+                    l = mid + 1;
+                } //if         
+            } ///if
+        } //while
+
+        return toReturn;
+    } //searchRange
+} //Solution
