d26e22ce3bfb0f1bedb8aa797d0fa15d14e8b916
Codey <forthecodeman@gmail.com>
2023-12-02 19:10:28 -0500

    Solved 1266

---
+++ 1266. Minimum Time Visiting All Points/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minTimeToVisitAllPoints(int[][] points) {
+        int total = 0;
+        for (int i = 1; i < points.length; i++) {
+            int xdist = Math.abs(points[i][0] - points[i-1][0]);
+            int ydist = Math.abs(points[i][1] - points[i-1][1]);
+            total += Math.max(xdist, ydist);
+        } //for
+        return total;
+    } //minTimeToVisitAllPoints
+} //Solution
