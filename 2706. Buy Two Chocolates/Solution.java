a688930022482607c650aefd70e8243ee5d9105a
Codey <forthecodeman@gmail.com>
2023-12-19 19:19:19 -0500

    Solved 2706

---
+++ 2706. Buy Two Chocolates/Solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        Arrays.sort(prices);
+        int n = prices[0] + prices[1];
+        if (n > money) return money;
+        return money - n;
+    } //buyChoco
+} //Solution
